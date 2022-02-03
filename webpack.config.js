7b341fd8a2f837711041893fb996027317b63837
Melissa Piper <mpiper@heysparkbox.com>
2022-02-03 12:22:10 -0500

    Get webpack up and running

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.tsx',
@@ -15,13 +14,11 @@ module.exports = {
   resolve: {
     extensions: ['.tsx', '.ts', '.js'],
   },
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
-    }),
-  ],
 };
