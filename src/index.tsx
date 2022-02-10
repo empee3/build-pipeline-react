a910b2e3f5fc3111cac53bba84c672962ea2f075
Melissa Piper <mpiper@heysparkbox.com>
2022-02-10 12:19:40 -0500

    chore: add scss to webpack

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Counter from './components/Counter';
 import ShowLogo from './components/ShowLogo';
+import './index.scss';
 
 ReactDOM.render(
-  <div>
-    <h1>Hello, Welcome to React and TypeScript</h1>
+  <div className="root">
+    <h1 className="h1">Hello, Welcome to React and TypeScript</h1>
     <Counter />
     <ShowLogo />
   </div>,
