373ba41498fe594d067a8df26a1107a0210b2579
Melissa Piper <mpiper@heysparkbox.com>
2022-02-14 10:06:24 -0500

    chore: make linting for TS less restrictive
    chore: make linting for TS less restrictive, fix scss path

--- src/components/ShowLogo.tsx
+++ src/components/ShowLogo.tsx
@@ -2,14 +2,6 @@ import * as React from 'react';
 
 const Logo = 'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/a2cpv3wvw7t0o4iybovk';
 
-export default class ShowLogo extends React.Component <{}> {
-  render() {
-    return (
-      <div>
-        <div>
-          <img height="250" src={Logo} alt="Sparkbox logo" />
-        </div>
-      </div>
-    );
-  }
-}
+const ShowLogo = () => <img height="250" src={Logo} alt="Sparkbox logo" />;
+
+export default ShowLogo;
