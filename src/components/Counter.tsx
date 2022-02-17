d422a5fc0dd912b97ec53b2f5618d137767d23e3
Melissa Piper <mpiper@heysparkbox.com>
2022-02-17 18:26:43 -0500

    feat: style components

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -19,10 +19,10 @@ export default class Counter extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>{this.state.count}</h1>
-        <button type="button" onClick={this.increment}>Increment</button>
-        <button type="button" onClick={this.decrement}>Decrement</button>
+      <div className="counter">
+        <h2 className="h2">{this.state.count}</h2>
+        <button type="button" className="button" onClick={this.increment}>Increment</button>
+        <button type="button" className="button" onClick={this.decrement}>Decrement</button>
       </div>
     );
   }
--- src/components/ShowLogo.tsx
+++ src/components/ShowLogo.tsx
@@ -2,6 +2,6 @@ import * as React from 'react';
 
 const Logo = 'https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,h_170,w_170,f_auto,b_white,q_auto:eco,dpr_1/a2cpv3wvw7t0o4iybovk';
 
-const ShowLogo = () => <img height="250" src={Logo} alt="Sparkbox logo" />;
+const ShowLogo = () => <img className="logo" src={Logo} alt="Sparkbox logo" />;
 
 export default ShowLogo;
